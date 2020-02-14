62ad75651a34de9f7ca82be1095f39c963049225
chloelau <cwl119@ic.ac.uk>
2020-02-14 12:35:26 +0000

    Round 2

---
+++ src/Grid.java
@@ -0,0 +1,26 @@
+public class Grid {
+  private int[][] grid;
+
+  public Grid() {
+    this.grid = new int[5][5];
+  }
+  
+  public void setAlive(int x, int y){
+    this.grid[x][y] = 1;
+  }
+  
+  public boolean checkAlive(int x, int y){
+    if (outOfGrid(x, y)) {
+      return false;
+    }
+    return this.grid[x][y] == 1;
+  }
+  
+  private boolean outOfGrid(i)
+  
+  public int countAliveNeighbors(int x, int y){
+    return 0;
+  }
+  
+  
+}
---
+++ tests/CodeRetreatTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CodeRetreatTest {
+  @Test
+  public void testCreateGrid() {
+    Grid grid = new Grid();
+    
+  }
+  
+  @Test
+  public void testAlive(){
+    Grid grid = new Grid();
+    grid.setAlive(2, 2);
+    assertTrue(grid.checkAlive(2, 2));
+  }
+  @Test (expected = IndexOutOfBoundsException.class)
+  public void testSetAliveOutOfBounds(){
+    Grid grid = new Grid();
+    grid.setAlive(-2, 2);
+  }
+  @Test
+  public void testCheckAliveOutOfBounds(){
+    Grid grid = new Grid();
+    assertEquals(0, grid.checkAlive(-2, 2));
+  }
+  
+  @Test
+  public void testCheckNeighbor1(){
+    Grid grid = new Grid();
+    assertEquals(0, grid.countAliveNeighbors(2, 2));
+  }
+}
