ba239385b8d8a2f7495938efbbb6ac8d82216285
chloelau <cwl119@ic.ac.uk>
2020-02-14 17:51:29 +0000

    6

--- src/Grid5.java
+++ src/Grid5.java
@@ -1,30 +1,30 @@
 public class Grid5 {
   int[][] grid;
 
-  Grid5(int x, int y){
+  Grid5(int x, int y) {
     grid = new int[x][y];
   }
-  
-  
+
   public void set(int x, int y, int set) {
     grid[x][y] = set;
   }
-  
+
   public int get(int x, int y) {
     return grid[x][y];
   }
-  
-  
+
   public int getNeighbors(int x, int y) {
     int counter = 0;
     for (int i = x - 1; i < x + 2; i++) {
-      for(int j = y - 1; j < y + 2; j++) {
-        if (i == x && j == y){
+      for (int j = y - 1; j < y + 2; j++) {
+        if (i == x && j == y || i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
           continue;
         }
-      counter += get(i,j);
+        counter += get(i, j);
       }
     }
     return counter;
   }
+  
+  
 }
--- tests/CodeRetreatTest5.java
+++ tests/CodeRetreatTest5.java
@@ -1,43 +1,45 @@
 import org.junit.Test;
-import org.junit.Assert.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class CodeRetreatTest5 {
-  
+
   @Test
-  public void testCreateGrid(){
-  Grid5 grid = new Grid5(5, 5);
-  
+  public void testCreateGrid() {
+    Grid5 grid = new Grid5(5, 5);
   }
-  
+
   @Test
-  public void testSet(){
+  public void testSet() {
     Grid5 grid = new Grid5(5, 5);
     grid.set(1, 2, 1);
   }
-  
+
   @Test
-  public void testGetAlive(){
-  
+  public void testGetAlive() {
+
     Grid5 grid = new Grid5(5, 5);
     grid.set(1, 2, 1);
     assertEquals(grid.get(1, 2), 1);
   }
-  
+
   @Test
-  public void testGetNeighbors(){
+  public void testGetNeighbors() {
     Grid5 grid = new Grid5(5, 5);
     grid.set(1, 2, 1);
     assertEquals(grid.getNeighbors(1, 2), 0);
   }
-  
+
   @Test
-  public void testGetNeighbors2(){
+  public void testGetNeighbors2() {
     Grid5 grid = new Grid5(5, 5);
     grid.set(1, 2, 1);
     assertEquals(grid.getNeighbors(1, 1), 1);
   }
-/*  @Test
-  public void testGetAlive(){}*/
+
+  @Test
+  public void testEdgeCases() {
+    Grid5 grid = new Grid5(5, 5);
+    assertEquals(grid.getNeighbors(0, 1), 0);
+  }
 }
